8ccde04c9b9dbd9129930739b33e5ec40519556e
Bharath <bharath.kristipati@gmail.com>
2017-09-21 08:47:02 +0530

    Added missing classes.

---
+++ src/test/java/com/slokam/testng/examples/ProfileTest.java
@@ -0,0 +1,11 @@
+package com.slokam.testng.examples;
+
+import org.testng.annotations.Test;
+
+public class ProfileTest {
+
+	@Test
+	public void test1(){
+		System.out.println("I am test1");
+	}
+}
---
+++ src/test/java/com/slokam/testng/examples/SikuliExample.java
@@ -0,0 +1,24 @@
+/*package com.slokam.testng.examples;
+
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Screen;
+import org.testng.annotations.Test;
+
+public class SikuliExample {
+
+	@Test
+	public void test1() {
+
+		Screen sc = new Screen();
+
+		try {
+			sc.wait("./src/test/resources/images/open.JPG",30);
+			sc.type("./src/test/resources/images/Type.JPG", "p3.docx");
+			sc.click("./src/test/resources/images/click.JPG", 0);
+		} catch (FindFailed e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+*/
